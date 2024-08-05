6366900ab14c3daf4fcb63ee76e0f9702643b81b
hassantimothy <hassantimothy422@gmail.com>
2024-08-06 00:19:34 +0300

    ...

--- tests/test_get_count.py
+++ tests/test_get_count.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+
+# test_get_count.py
+#!/usr/bin/python3
+
+# test_get_count.py
 """ Test .get() and .count() methods
 """
 from models import storage
 from models.state import State
 
-print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
+def test_get_count():
+    # Print the count of all objects and State objects
+    print("All objects: {}".format(storage.count()))
+    print("State objects: {}".format(storage.count(State)))
 
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+    # Try to get the first state object
+    states = list(storage.all(State).values())
+    if states:
+        first_state_id = states[0].id
+        print("First state: {}".format(storage.get(State, first_state_id)))
+    else:
+        print("No State objects found.")
+
+if __name__ == "__main__":
+    test_get_count()
